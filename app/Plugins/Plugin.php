37f9cd09fb04a16b77b6ca15a402512ee00fd5d6
ManaPHP Mark <manaphp@qq.com>
2021-03-01 21:40:14 +0800

    Modified   app/Commands/Command.php Added      app/Plugins/Plugin.php Added      app/Services/Service.php

---
+++ app/Plugins/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Plugins;
+
+/**
+ * @property-read \ManaPHP\Logging\LoggerInterface       $logger
+ * @property-read \Redis                                 $redisDb
+ * @property-read \Redis                                 $redisCache
+ * @property-read \Redis                                 $redisBroker
+ */
+class Plugin extends \ManaPHP\Plugin
+{
+
+}
---
+++ app/Services/Service.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * @property-read \ManaPHP\Logging\LoggerInterface       $logger
+ * @property-read \Redis                                 $redisDb
+ * @property-read \Redis                                 $redisCache
+ * @property-read \Redis                                 $redisBroker
+ */
+class Service extends \ManaPHP\Service
+{
+
+}
